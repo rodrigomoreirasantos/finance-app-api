ad73f146ea614c02a5e179591b5982fadb40e0e3
rodrigo <rodrigomoreirasantos01@gmail.com>
2023-10-24 07:52:22 -0400

    feat: add helper function to check if id is valid

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,12 +1,12 @@
 import { GetUserByIdUseCase } from '../use-cases/get-user-by-id.js'
 import { notFound, ok, serverError } from './helpers/http.js'
-import validator from 'validator'
-import { invaldIdResponse } from './helpers/user.js'
+import { checkIfIdIsValid, invaldIdResponse } from './helpers/user.js'
 
 export class GetUserByIdController {
     async execute(httpRequest) {
         try {
-            const isIdValid = validator.isUUID(httpRequest.params.userId)
+            const userId = httpRequest.params.userId
+            const isIdValid = checkIfIdIsValid(userId)
 
             if (!isIdValid) {
                 return invaldIdResponse()
