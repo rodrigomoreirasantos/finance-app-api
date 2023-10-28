e099a51e2467705b32d5c581939252bebcff4754
rodrigo <rodrigomoreirasantos01@gmail.com>
2023-10-28 11:54:02 -0400

    feat: add Dependencey Injection in GetUserById

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,4 +1,3 @@
-import { GetUserByIdUseCase } from '../use-cases/index.js'
 import {
     checkIfIdIsValid,
     invaldIdResponse,
@@ -8,6 +7,9 @@ import {
 } from './helpers/index.js'
 
 export class GetUserByIdController {
+    constructor(getUserByIdUseCase) {
+        this.getUserByIdUseCase = getUserByIdUseCase
+    }
     async execute(httpRequest) {
         try {
             const userId = httpRequest.params.userId
@@ -17,8 +19,7 @@ export class GetUserByIdController {
                 return invaldIdResponse()
             }
 
-            const getUserByIdUseCase = new GetUserByIdUseCase()
-            const user = await getUserByIdUseCase.execute(
+            const user = await this.getUserByIdUseCase.execute(
                 httpRequest.params.userId,
             )
             if (!user) {
