5db2fb185c3eaef5de5a8347c3615a1e17b9f499
rodrigo <rodrigomoreirasantos01@gmail.com>
2023-11-13 11:58:31 -0500

    fix: fix CreateTransactionController required filds validation

--- src/controllers/transaction/create-transaction.js
+++ src/controllers/transaction/create-transaction.js
@@ -16,17 +16,13 @@ export class CreateTransactionController {
             const params = httpRequest.body
 
             // validar a requisição (campos obrigatório, tamanho de senha e email)
-            const requiredFields = [
-                'id',
-                'user_id',
-                'name',
-                'date',
-                'amount',
-                'type',
-            ]
+            const requiredFields = ['user_id', 'name', 'date', 'amount', 'type']
 
             for (const field of requiredFields) {
-                if (!params[field] || params[field].trim().length === 0) {
+                if (
+                    !params[field] ||
+                    params[field].toString().trim().length === 0
+                ) {
                     return badRequest({ message: `Missing params: ${field}` })
                 }
             }
